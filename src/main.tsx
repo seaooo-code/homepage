9ccda89ab5cc259d40ad8ab8b3f61ae662d626d7
seaooo <chenhui.yang@seaooo.com>
2025-11-28 19:44:56 +0800

    :sparkles: 引入 Speed Insights 支持并更新依赖

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import App from "./App.tsx";
 
+injectSpeedInsights();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
